f9e8a1894391964d24ce594b2a1f9e5fd0df833c
Aidan Sweeny <apswee22@colby.edu>
2020-07-21 18:15:45 -0400

    Added all necessary files

--- schema.sql
+++ schema.sql
@@ -5,29 +5,24 @@ CREATE DATABASE employeeTrackDB;
 USE employeeTrackDB;
 
 CREATE TABLE department (
-  id INT NOT NULL AUTO_INCREMENT,
-  name VARCHAR(30),
-  PRIMARY KEY (id)
+    id INT NOT NULL AUTO_INCREMENT,
+    name VARCHAR(30),
+    PRIMARY KEY (id)
 );
 
 CREATE TABLE role (
-  id INT NOT NULL AUTO_INCREMENT,
-  title VARCHAR(30),
-  
-  PRIMARY KEY (id)
+    id INT NOT NULL AUTO_INCREMENT,
+    title VARCHAR(30),
+    salary DECIMAL,
+    department_id INT,
+    PRIMARY KEY (id)
 );
 
-INSERT INTO songs (title, artist, genre)
-VALUES ("Despacito", "Justin  Beiber", "Pop");
-
-INSERT INTO songs (title, artist, genre)
-VALUES ("Jump", "Van Halen", "Rock");
-
-INSERT INTO songs (title, artist, genre)
-VALUES ("Break on Through", "Doors", "Rock");
-
-INSERT INTO songs (title, artist, genre)
-VALUES ("God's Plan", "Drake", "Rap");
-
-INSERT INTO songs (title, artist, genre)
-VALUES ("Chicken Fried", "Zac Brown Band", "Country");
+CREATE TABLE employee (
+    id INT NOT NULL AUTO_INCREMENT,
+    first_name VARCHAR(30),
+    last_name VARCHAR(30),
+    role_id INT,
+    manager_id INT,
+    PRIMARY KEY (id)
+);
